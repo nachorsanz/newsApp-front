be6365249a7d82ad163455e68b06b9420be0e1c2
VanLuarK <vanluark99@gmail.com>
2022-08-29 12:20:43 +0200

    Solved ts errors

--- components/Articles/ArticlesComponent.tsx
+++ components/Articles/ArticlesComponent.tsx
@@ -1,24 +1,36 @@
-import Image from "next/image"
-import { ReactElement } from "react"
+import Image from "next/image";
+import { ReactElement } from "react";
 
 type Articulo = {
-    author: string,
-    title: string,
-    description: string,
-    urlToImage: string,
-   }
-export default function Articles (articles : Articulo[]): ReactElement{
-  return  (
-  <div>
-    {articles?.map((article, index)=> 
-      (<article key={index}>
-        <Image src={  article.urlToImage} alt={`title ${article.title}`} width={450} height={300} layout="responsive" />
-      <h1>{article.title}</h1>
-      <h4>{article.author || "Agency"}</h4>
-      <p>{article.description}</p>
+  author: string;
+  title: string;
+  description: string;
+  urlToImage: string;
+};
 
-    </article>)
-    )}
-    </div>
-    )
+interface Props {
+  articles: Articulo[];
 }
+
+const Articles = ({ articles }: Props) => {
+  return (
+    <div>
+      {articles?.map((article, index) => (
+        <article key={index}>
+          <Image
+            src={article.urlToImage}
+            alt={`title ${article.title}`}
+            width={450}
+            height={300}
+            layout="responsive"
+          />
+          <h1>{article.title}</h1>
+          <h4>{article.author || "Agency"}</h4>
+          <p>{article.description}</p>
+        </article>
+      ))}
+    </div>
+  );
+};
+
+export default Articles;
--- pages/PageLayout.tsx
+++ pages/PageLayout.tsx
@@ -1,11 +1,23 @@
 import Head from "next/head";
-import * as nextTypes from "next/types"
-import { FC, ReactNode, ReactPortal } from "react";
-export default function PageLayout  (children : ReactNode, title: string){
-    return <>
-    <Head>
-        <title>{title}</title>
-    </Head>
-    <main>{children}</main>
-    </>
+import { FunctionComponent } from "react";
+
+interface PageLayoutProps {
+  title: string;
+  children: JSX.Element;
 }
+
+const PageLayout: FunctionComponent<PageLayoutProps> = ({
+  children,
+  title,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default PageLayout;
