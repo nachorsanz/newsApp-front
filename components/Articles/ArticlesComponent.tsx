f978cc9daad1a84107f3b3290ff5e229e1339dfc
VanLuarK <vanluark99@gmail.com>
2022-08-30 10:12:32 +0200

    Styling home page

--- components/Articles/ArticlesComponent.tsx
+++ components/Articles/ArticlesComponent.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Article } from "../../utils/types";
+import styles from "../../styles/Article.module.css";
 
 interface ArticlesProps {
   articles: Article[];
@@ -7,9 +8,9 @@ interface ArticlesProps {
 
 const Articles = ({ articles }: ArticlesProps) => {
   return (
-    <div>
+    <div className={styles.container}>
       {articles?.map((article, index) => (
-        <article key={index}>
+        <article key={index} className={styles.new}>
           <Image
             src={article.urlToImage}
             alt={`title ${article.title}`}
